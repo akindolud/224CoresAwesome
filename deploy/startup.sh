5d74dee16a2b41729c6360179a57bf5bbfb066e2
akindolu <56455590+akindolu@users.noreply.github.com>
2020-06-18 11:36:23 -0400

    Second commit, Included 2 new files and modified two old files. 1. Included a deploy/machineTypesList.json that contains a list of availabe machine types. 2. Incuded a deploy/startup.sh file that sets up the vm with required applications 3. Modified the README.md file to incude description of the new files 4. Modified the deploy/vmconfig.yaml file to use the deploy/startup.sh script

---
+++ deploy/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Update the packages on the vm
+echo "Updating packages "
+sudo apt-get update
+
+# Install required software on the vm
+# Install python3
+sudo apt-get install python3
+
+# Install python3 pip
+sudo apt-get install python3-pip
+
+# Install and setup OpenFOAM and Paraview
+
+# Install and setup Gmsh
+
+# 
