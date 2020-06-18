588fa8a3715c12ef447e25547de535c899ccabec
akindolu <56455590+akindolu@users.noreply.github.com>
2020-06-18 08:54:46 -0400

    First commit, this shoud work but has not been tested.

---
+++ README.md
@@ -0,0 +1,15 @@
+# DESCRIPTION
+
+### What is it?
+
+This is a project that automatically provisions a compute engine for high performance computing (HPC) applications. It uses Google's cloud platform.
+
+### Main Components
+
+The main components of this project are:
+
+* A shell script "deploy.sh": this handles the entire process of provisioning the resources and deleting it after a set time.
+* A yaml config file that holds the configuration information for the compute engine.
+
+## License
+This work is published under the terms of MIT License. See LICENSE file for the terms of this license.
---
+++ deploy/deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# A shell script to create and delete a deployment on GCP.
+# To use this file, set the required variables and run using bash in GCP cloud shell
+
+# Create variable name for GCP deployment
+DEPLOYMENT_NAME="test-deployment"
+SLEEP_SECONDS=600
+
+# Print a display message to show the shell script is now running
+echo "0. Script running, your deployment will be created in several stages and might take a few minutes"
+
+# Set your project to that defined in the DEVSHELL_PROJECT_ID environment variable
+echo "1. Now setting the project to " $DEVSHELL_PROJECT_ID
+gcloud config set project $DEVSHELL_PROJECT_ID
+
+# Enable the following gcloud services
+# 1. compute engine api
+# 2. deployment manager
+echo "2. Now enabling the compute and deployment manager services"
+gcloud services enable compute.googleapis.com
+gcloud services enable deploymentmanager.googleapis.com
+
+# Use 'sed' to set the [PROJECT_ID] and [DEPLOYMENT_NAME] in the .yaml config file
+echo "3. Now setting the project ID and deployment name in the your config file"
+sed -i "s/[PROJECT_ID]/$DEVSHELL_PROJECT_ID/g" vmconfig.yaml
+sed -i "s/[DEPLOYMENT_NAME]/$DEPLOYMENT_NAME/g" vmconfig.yaml
+
+# Your resources will be deployed on GCP in this step using the specs in the config file
+echo "4. Now deploying your resources on GCP, please wait for it to complete..."
+gcloud deployment-manager deployments create $DEPLOYMENT_NAME --config vmconfig.yaml
+echo "Deployment complete, showing details"
+gcloud deployment-manager deployments describe $DEPLOYMENT_NAME
+
+# Wait or carryout some actions before deleting deployment
+echo "Sleeping for 15 minutes before deleting resources"
+sleep $SLEEP_SECONDS
+
+# Your resources will be deleted in this section.
+echo "Now deleting resources on GCP"
+gcloud deployment-manager deployments delete $DEPLOYMENT_NAME
